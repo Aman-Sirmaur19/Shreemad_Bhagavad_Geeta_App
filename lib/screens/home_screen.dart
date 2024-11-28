d7b99feced3d998abe167f518dd5d0385851dbf3
Aman-Sirmaur19 <amansirmaur190402@gmail.com>
2024-11-28 20:40:58 +0530

    Implemented in-app update.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:in_app_update/in_app_update.dart';
 
 import '../services/api_service.dart';
 import '../widgets/main_drawer.dart';
@@ -19,11 +22,100 @@ class _HomeScreenState extends State<HomeScreen> {
   final ApiService apiService = ApiService();
   late Future<List<dynamic>> chapters;
   final FlutterTts textToSpeech = FlutterTts();
+  bool isBannerLoaded = false;
+  late BannerAd bannerAd;
+  bool isInterstitialLoaded = false;
+  late InterstitialAd interstitialAd;
 
   @override
   void initState() {
     super.initState();
+    _checkForUpdate();
+    _initializeBannerAd();
+    _initializeInterstitialAd();
     chapters = apiService.fetchChapters();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    bannerAd.dispose();
+    interstitialAd.dispose();
+  }
+
+  Future<void> _checkForUpdate() async {
+    log('Checking for Update!');
+    await InAppUpdate.checkForUpdate().then((info) {
+      setState(() {
+        if (info.updateAvailability == UpdateAvailability.updateAvailable) {
+          log('Update available!');
+          update();
+        }
+      });
+    }).catchError((error) {
+      log(error.toString());
+    });
+  }
+
+  void update() async {
+    log('Updating');
+    await InAppUpdate.startFlexibleUpdate();
+    InAppUpdate.completeFlexibleUpdate().then((_) {}).catchError((error) {
+      log(error.toString());
+    });
+  }
+
+  void _initializeBannerAd() async {
+    bannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: 'ca-app-pub-9389901804535827/5411361970',
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            isBannerLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+          isBannerLoaded = false;
+          log(error.message);
+        },
+      ),
+      request: const AdRequest(),
+    );
+    bannerAd.load();
+  }
+
+  void _initializeInterstitialAd() async {
+    InterstitialAd.load(
+      adUnitId: 'ca-app-pub-9389901804535827/8281628976',
+      request: const AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (ad) {
+          interstitialAd = ad;
+          setState(() {
+            isInterstitialLoaded = true;
+          });
+          interstitialAd.fullScreenContentCallback = FullScreenContentCallback(
+            onAdDismissedFullScreenContent: (ad) {
+              ad.dispose();
+              _initializeInterstitialAd();
+            },
+            onAdFailedToShowFullScreenContent: (ad, error) {
+              log('Ad failed to show: ${error.message}');
+              ad.dispose();
+              _initializeInterstitialAd();
+            },
+          );
+        },
+        onAdFailedToLoad: (error) {
+          log('Failed to load interstitial ad: ${error.message}');
+          setState(() {
+            isInterstitialLoaded = false;
+          });
+        },
+      ),
+    );
   }
 
   void _speak(String text) async {
@@ -45,6 +137,9 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           )),
       drawer: const MainDrawer(),
+      bottomNavigationBar: isBannerLoaded
+          ? SizedBox(height: 50, child: AdWidget(ad: bannerAd))
+          : null,
       body: FutureBuilder<List<dynamic>>(
         future: chapters,
         builder: (context, snapshot) {
@@ -60,10 +155,13 @@ class _HomeScreenState extends State<HomeScreen> {
                 final chapterName = utf8.decode(chapter['name'].runes.toList());
                 return Card(
                   child: ListTile(
-                    onTap: () => Navigator.push(
-                        context,
-                        CupertinoPageRoute(
-                            builder: (_) => TabScreen(chapter: chapter))),
+                    onTap: () {
+                      if (isInterstitialLoaded) interstitialAd.show();
+                      Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (_) => TabScreen(chapter: chapter)));
+                    },
                     leading: CircleAvatar(
                         child: Text('${chapter['chapter_number']}')),
                     title: Text(
